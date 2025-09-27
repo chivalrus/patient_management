b2263b130ce5b2ee379cfdec1855b8c730ce42fe
chivalrus <darylalbero@gmail.com>
2025-09-27 14:38:38 +0800

    Add separator

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,17 +1,29 @@
 import { RegistrationForm } from "@/components/registration-form";
 import { SearchPatient } from "@/components/search-patient";
+import { Separator } from "@radix-ui/react-separator";
 import { useState } from "react";
 
 const Home = () => {
     const [registered, setRegistered] = useState<boolean>(false);
 
     return (
-        <div className="grid grid-cols-12 gap-4 w-full">
-            <div className="col-span-4">
-                <RegistrationForm setRegistered={setRegistered}/>
+        <div>
+            <div className="grid grid-cols-12 gap-4 w-full">
+                <div className="space-y-1 col-span-12">
+                    <h4 className="text-2xl leading-none font-medium">Patient Mangement System</h4>
+                    <p className="text-muted-foreground text-sm">
+                    Simple record management for patient info
+                    </p>
+                </div>
             </div>
-            <div className="col-span-8 min-h-[485px]">
-                <SearchPatient newRegistered={registered} setNewRegistered={setRegistered}/>
+            <Separator className="my-4 border-1" />
+            <div className="grid grid-cols-12 gap-4 w-full">
+                <div className="col-span-4">
+                    <RegistrationForm setRegistered={setRegistered}/>
+                </div>
+                <div className="col-span-8 min-h-[485px]">
+                    <SearchPatient newRegistered={registered} setNewRegistered={setRegistered}/>
+                </div>
             </div>
         </div>
     );
